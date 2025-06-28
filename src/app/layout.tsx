811e5dee3950a48ce94207737a0110d0398a4f1c
Kshitij Koranne <kshitij.koranne@live.com>
2025-06-28 22:52:02 +0530

    Remove Google verification file and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -157,6 +157,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="SIhSFzG-_wB5brnoEV_CRi-ovHUfjB-lJ9ziBDyJYeQ" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
